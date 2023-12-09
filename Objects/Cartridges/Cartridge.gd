dd3a8cdf16a0873e1bf05f7f765f0ab22d2b6361
Artturi <asvehv@utu.fi>
2023-12-09 15:56:57 +0200

    Muutettu gitignore

--- Objects/Cartridges/Cartridge.gd
+++ Objects/Cartridges/Cartridge.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 class_name Cartridge
 
 enum MODES {DEFAULT, JUMP, PUSH, SPEED}
-var mode: MODES = MODES.JUMP
+@export var mode: MODES
 
 func _on_area_2d_body_entered(body):
 	if body is Player:
