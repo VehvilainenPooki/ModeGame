89c1dd4912b4e58eb52f7a1cb099f4144270881f
Artturi <asvehv@utu.fi>
2023-12-10 18:11:56 +0200

    Jatkettu työntämistä

--- Objects/Crate/Crate.gd
+++ Objects/Crate/Crate.gd
@@ -4,7 +4,7 @@ class_name Crate
 var player: Player
 var moving: bool
 enum MODES {DEFAULT, JUMP, PUSH, SPEED}
-can_sleep = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	moving = false
@@ -13,16 +13,17 @@ func _on_area_2d_body_entered(body):
 	if body is Player and body.pushing:
 		player = body
 		moving = true
+		$".".apply_impulse(Vector2(0,0))
+		print(moving)
 
 func _on_area_2d_body_exited(body):
 	if body is Player:
 		moving = false
+		print(moving)
 
 
 func _integrate_forces(state):
 	if moving:
 		print("jou")
-		state.apply_force(Vector2(1000,-10))
-	
-
+		state.apply_central_force(Vector2(1000,-10))
 
